22d0bfbbf1c0461a7a245ddf6431a23dadc6b2d6
BEGARI VIVEK <begarivivek630@gmail.com>
2025-08-20 23:31:03 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (85.51%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int p1=Math.abs(z-x);
+        int p2=Math.abs(z-y);
+        if(p1<p2) return 1;
+        if(p2<p1) return 2;
+        else return 0;
+    }
+}
