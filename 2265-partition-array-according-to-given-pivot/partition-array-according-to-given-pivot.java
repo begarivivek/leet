11b936ddd0d420a45abfb84127aa4c7fbe105b6c
BEGARI VIVEK <begarivivek630@gmail.com>
2025-08-27 22:43:32 +0530

    Time: 5 ms (76.73%) | Memory: 68.8 MB (8.92%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,40 +1,24 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int n=nums.length;
-        int lp[]=new int[n];
-        int gp[]=new int[n];
-        int ans[]=new int[n];
-        int c=0;
-        int lpIndx=0,gpIndx=0;
-
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]<pivot)
-            {
-                lp[lpIndx++]=nums[i];
-                
-            }
-            if(nums[i]==pivot) c++;
-            else if(nums[i]>pivot)
-            {
-                gp[gpIndx++]=nums[i];
+        int n = nums.length;
+        int[] ans = new int[n];
+        int idx = 0;
+        for (int num : nums) {
+            if (num < pivot) {
+                ans[idx++] = num;
             }
         }
-        for(int i=1;i<=c;i++)
-        {
-            lp[lpIndx++]=pivot;
+        for (int num : nums) {
+            if (num == pivot) {
+                ans[idx++] = num;
+            }
         }
-        int idx=0;
-        for(int i=0;i<lpIndx;i++)
-        {
-            nums[idx++]=lp[i];
+        for (int num : nums) {
+            if (num > pivot) {
+                ans[idx++] = num;
+            }
         }
-        for(int i=0;i<gpIndx;i++)
-        {
-            nums[idx++]=gp[i];
-        }
-        return nums;
 
-
+        return ans;
     }
 }
