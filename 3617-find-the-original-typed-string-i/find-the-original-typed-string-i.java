ebadbb5bf3cd98f59d9eb41b709af1ee0cda12da
BEGARI VIVEK <begarivivek630@gmail.com>
2025-09-15 21:18:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (26.97%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
@@ -3,7 +3,7 @@ class Solution {
         int res=1;
         for(int i=1;i<word.length();i++)
         {
-            if(word.charAt(i-1)==word.charAt(i)) res++;
+            if(word.charAt(i)==word.charAt(i-1)) res++;
         }
         return res;
     }
