6957578262f3a97a19a1d59a8c1f79741967b0c5
BEGARI VIVEK <begarivivek630@gmail.com>
2025-10-10 15:34:05 +0530

    Time: 20 ms (6.88%) | Memory: 45.2 MB (36.25%) - LeetSync

--- 2350-find-closest-number-to-zero/find-closest-number-to-zero.java
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.java
@@ -1,23 +1,12 @@
 class Solution {
     public int findClosestNumber(int[] nums) {
-       int n= nums.length;
-       int closestNum=nums[0];
-
-       for(int i=1;i<n;i++)
-       {
-        int curr=nums[i];
-        if(Math.abs(curr)<Math.abs(closestNum))
-        {
-            closestNum=curr;
-        }
-        else if(Math.abs(curr)==Math.abs(closestNum) && curr>closestNum)
-        {
-            closestNum=curr;
-        }
-       }
-
-       return closestNum;
-       
-        
+        List<int[]> l = new ArrayList<>();
+        for(var i : nums)
+            l.add(new int[]{i,Math.abs(i)});
+        Collections.sort(l,(a,b)->a[1]-b[1]);
+        int min = l.get(0)[1], res = l.get(0)[0];
+        for(var  i : l)
+            if(res < i[0] && min == i[1])res=i[0];
+       return res;
     }
 }
