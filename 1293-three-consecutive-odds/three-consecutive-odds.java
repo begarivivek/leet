dda995aa0101122d706b870f5d71aec811247bf4
BEGARI VIVEK <begarivivek630@gmail.com>
2025-10-14 14:10:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (23.07%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.java
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -1,9 +1,18 @@
 class Solution {
-    public boolean threeConsecutiveOdds(int[] nums) {
-        for(int i=0;i<=nums.length-3;i++)
-        {
-            if(nums[i]%2==1 && nums[i+1]%2==1 && nums[i+2]%2==1) return true;
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==1){
+                count++;
+                if(count==3){
+                    return true;
+                }
+            }
+            else{
+                count=0;
+            }
         }
+
         return false;
     }
 }
