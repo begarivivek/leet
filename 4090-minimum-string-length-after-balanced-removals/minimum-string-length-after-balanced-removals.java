4ee590023d80e54cd3f296a3f26745bada7df317
BEGARI VIVEK <begarivivek630@gmail.com>
2025-11-16 10:39:09 +0530

    Time: 3 ms (100.00%) | Memory: 46.9 MB (0.00%) - LeetSync

---
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minLengthAfterRemovals(String s) {
+        int ac=0,bc=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='a') ac++;
+            else bc++;
+        }
+        if(ac==bc) return 0;
+        if(bc==0) return ac;
+        return Math.abs(ac-bc);
+    }
+}
