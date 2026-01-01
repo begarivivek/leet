fe5738ec734c6180c5ceb1b3455e0fa85d3bab89
BEGARI VIVEK <begarivivek630@gmail.com>
2026-01-01 23:15:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (94.31%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] plusOne(int[] d) {
+       
+      for(int i=d.length-1;i>=0;i--){
+        if(d[i]<9){
+            d[i]++;
+            return d;
+        }
+        d[i]=0;  
+      }
+d=new int[d.length+1];
+d[0]=1;
+return d;
+    }
+}
