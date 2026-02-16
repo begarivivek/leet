085e660e842788910183327d61a29c35062a1178
BEGARI VIVEK <begarivivek630@gmail.com>
2026-02-16 10:29:25 +0530

    Time: 8 ms (0.00%) | Memory: 46.8 MB (100.00%) - LeetSync

--- 4212-toggle-light-bulbs/toggle-light-bulbs.java
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.java
@@ -2,8 +2,8 @@ class Solution {
     public List<Integer> toggleLightBulbs(List<Integer> bulbs) {
         Set<Integer> set = new HashSet<>();
         for(int i:bulbs){
-           if(set.contains(i)) set.remove(i);
-           else set.add(i);
+           if(!set.contains(i)) set.add(i);
+           else set.remove(i);
         }
         List<Integer> result = new ArrayList<>(set);
         Collections.sort(result);
