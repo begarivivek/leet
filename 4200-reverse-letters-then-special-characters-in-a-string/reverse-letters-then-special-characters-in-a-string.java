51bf08d175ea9781d539a4ec14e0b2cd74c29848
BEGARI VIVEK <begarivivek630@gmail.com>
2026-02-17 14:44:09 +0530

    Time: 3 ms (57.61%) | Memory: 44.6 MB (85.09%) - LeetSync

---
+++ 4200-reverse-letters-then-special-characters-in-a-string/reverse-letters-then-special-characters-in-a-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String reverseByType(String s) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(Character.isLetter(ch)){
+                sb1.append(ch);
+            }
+            else{
+                sb2.append(ch);
+            }
+        }
+        sb1.reverse();
+        sb2.reverse();
+
+        StringBuilder ans= new StringBuilder();
+        int l=0, cc=0;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(Character.isLetter(c)){
+                ans.append(sb1.charAt(l++));
+            }
+            else ans.append(sb2.charAt(cc++));
+        }
+        return ans.toString();
+    }
+}
