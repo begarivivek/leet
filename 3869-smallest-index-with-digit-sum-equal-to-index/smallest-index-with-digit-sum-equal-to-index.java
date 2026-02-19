fa7476887f9e473b97de576e38445071dc3cfd33
BEGARI VIVEK <begarivivek630@gmail.com>
2026-02-19 22:07:46 +0530

    Time: 1 ms (100.00%) | Memory: 45.5 MB (90.14%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
@@ -1,25 +1,21 @@
 class Solution {
     public int smallestIndex(int[] nums) {
-        int nn = 0, d, sum;
-        int min = Integer.MAX_VALUE;
+
         for (int i = 0; i < nums.length; i++) {
-            sum = 0;
-            d = 0;
-            nn = nums[i];
+
+            int sum = 0;
+            int nn = nums[i];
+
             while (nn > 0) {
-                d = nn % 10;
-                sum += d;
+                sum += nn % 10;
                 nn = nn / 10;
             }
+
             if (sum == i) {
-                if (sum < min) {
-                    min = Math.min(min, sum);
-                }
+                return i;
             }
         }
-        if (min == Integer.MAX_VALUE) {
-            return -1;
-        }
-        return min;
+
+        return -1;
     }
 }
