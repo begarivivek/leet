9bba2f3df3f23f9066a6216ed26629fb36c41a82
BEGARI VIVEK <begarivivek630@gmail.com>
2026-03-14 23:03:44 +0530

    Time: 36 ms (12.50%) | Memory: 83.1 MB (23.56%) - LeetSync

--- 2355-maximum-consecutive-floors-without-special-floors/maximum-consecutive-floors-without-special-floors.java
+++ 2355-maximum-consecutive-floors-without-special-floors/maximum-consecutive-floors-without-special-floors.java
@@ -1,14 +1,11 @@
 class Solution {
     public int maxConsecutive(int bottom, int top, int[] special) {
         Arrays.sort(special);
-        int ans = special[0] - bottom;
+        int ans = Math.max(special[0] - bottom, top - special[special.length - 1]);
 
         for (int i = 1; i < special.length; i++) {
             ans = Math.max(ans, special[i] - special[i - 1] - 1);
         }
-
-        ans = Math.max(ans, top - special[special.length - 1]);
-
         return ans;
     }
 }
